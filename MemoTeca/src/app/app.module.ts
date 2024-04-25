61c6d509716decccea89e85e4cbb5ead833037f1
Raul Lima <raul.novaes@f1rst.com.br>
2024-04-25 20:40:16 -0300

    Adicionando atributos de navegação na aplicação

--- MemoTeca/src/app/app.module.ts
+++ MemoTeca/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { FormsModule } from '@angular/forms';
+import { ListarPensamentosComponent } from './components/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CriarPensamentoComponent
+    CriarPensamentoComponent,
+    ListarPensamentosComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [RouterOutlet],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
